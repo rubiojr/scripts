87b8af4b6a3f1159713be40454beaf7499e9751d
Sergio Rubio <rubiojr@frameos.org>
2013-11-03 23:24:54 +0100

    add user ssh pubkey

--- create-luser-container.sh
+++ create-luser-container.sh
@@ -2,9 +2,10 @@
 set -e
 LUSER=$1
 ROOTFS="/var/lib/lxc/user-$LUSER/rootfs"
+SSHKEY=$2
 
 [ -z "$LUSER" ] && {
-  echo "Usage: $0 <username>"
+  echo "Usage: $0 <username> [ssh_key]"
   exit 1
 }
 
@@ -23,3 +24,20 @@ EOF
 
 chroot $ROOTFS useradd --shell /bin/bash $LUSER
 chroot $ROOTFS userdel -f ubuntu
+
+[ -z "$SSHKEY" ] || {
+  mkdir -p $ROOTFS/home/$LUSER/.ssh
+
+  if [[ $SSHKEY =~ ^http ]]; then
+    curl --silent $SSHKEY > $ROOTFS/home/$LUSER/.ssh/authorized_keys
+  else
+    if [ -f "$SSHKEY" ]; then
+      cat $SSHKEY > $ROOTFS/home/$LUSER/.ssh/authorized_keys
+    else
+      echo Invalid SSH key file
+      exit 1
+    fi
+  fi
+  chroot $ROOTFS chown $LUSER:$LUSER /home/$LUSER -R
+  chmod 700 $ROOTFS/home/$LUSER/.ssh
+}
