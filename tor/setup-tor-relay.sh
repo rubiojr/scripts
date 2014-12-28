706424de20102539ca835630397ad262f962c4bb
Sergio Rubio <rubiojr@frameos.org>
2014-12-28 13:16:13 +0100

    Added rely setup script

---
+++ tor/setup-tor-relay.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+set -e
+
+if [ `whoami` != 'root' ]; then
+  echo "This script requires root."
+  exit 1
+fi
+
+apt-key list | grep -q 886DDD89 || {
+  gpg --keyserver keys.gnupg.net --recv 886DDD89
+  gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | apt-key add -
+}
+
+cat > /etc/apt/sources.list.d/tor.list <<EOF
+deb     http://deb.torproject.org/torproject.org $(lsb_release -c -s) main
+EOF
+
+apt-get update
+apt-get install -y unattended-upgrades ntp
+
+/etc/init.d/ntp stop || true
+
+echo "Etc/UTC" > /etc/timezone
+dpkg-reconfigure -f noninteractive tzdata
+ntpdate pool.ntp.org
+
+service cron restart
+service rsyslog restart
+service ntp start
+
+cat > /etc/apt/apt.conf.d/20auto-upgrades <<EOF
+APT::Periodic::Update-Package-Lists "1";
+APT::Periodic::Unattended-Upgrade "1";
+EOF
+
+apt-get install -y deb.torproject.org-keyring tor tor-arm
+
+cat > /etc/tor/torrc <<EOF
+ORPort 9001
+# Nickname mynick
+RelayBandwidthRate 1000 KB  # Throttle traffic to 1000KB/s (800Kbps)
+RelayBandwidthBurst 2000 KB # But allow bursts up to 2000KB/s (1600Kbps)
+
+AccountingMax 1000 GB
+AccountingStart month 1 01:00
+
+# ContactInfo Super Foo <superfoo@mydomainname.random>
+## You might also include your PGP or GPG fingerprint if you have one:
+#ContactInfo 0xFFFFFFFF Random Person <nobody AT example dot com>
+
+DirPort 9030 # what port to advertise for directory connections
+## Uncomment this if you run more than one Tor relay, and add the identity
+## key fingerprint of each Tor relay you control, even if they're on
+## different networks. You declare it here so Tor clients can avoid
+## using more than one of your relays in a single circuit. See
+## https://www.torproject.org/docs/faq#MultipleRelays
+## However, you should never include a bridge's fingerprint here, as it would
+## break its concealability and potentionally reveal its IP/TCP address.
+#MyFamily keyid,keyid,...
+
+ExitPolicy reject *:* # no exits allowed
+EOF
+
+service tor restart
+
+echo OK
