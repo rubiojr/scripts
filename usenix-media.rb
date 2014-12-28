eac25e91670e82a21e8b166aaa49d7724d9afbd7
Sergio Rubio <rubiojr@frameos.org>
2014-12-28 18:08:16 +0100

    Save nfo file with the video

--- usenix-media.rb
+++ usenix-media.rb
@@ -10,36 +10,42 @@ agent = Mechanize.new
 agent.user_agent_alias = 'Mac Safari'
 agent.pluggable_parser.default = Mechanize::Download
 
-def save_file(url, path)
+def save_file(url, path, title)
   puts "Saving file #{path}"
-  FileUtils.mkdir_p(File.dirname(path))
-  link_file = path + '.link'
+
+  dir = File.dirname(path)
+  ext = File.extname(path)
+  fname = File.basename(path, ext)
+  FileUtils.mkdir_p(dir)
+
+  nfo_file = File.join(dir, "#{fname}.nfo")
 
   # save the URL from where the file was downloaded
-  unless File.exist?(link_file)
-    File.open(path + '.link', 'w') { |f| f.puts url }
+  unless File.exist?(nfo_file)
+    File.open(nfo_file, 'w') do |f|
+      f.puts "<movie>\n<title>\n#{title}\n</title>\n</movie>"
+      f.puts url
+    end
   end
 
   `curl -# -C - --retry 2 -o #{path} #{url}`
 end
 
 def fetch_video(agent, url)
-  puts "Fetching video form page #{url}"
-
   presentations = agent.get(url).links.find_all do |l|
     l.uri.to_s =~ /presentation/
   end
 
   presentations.each do |p|
     page = agent.get(p.uri.to_s)
-    video_name= page.title.gsub!(/[^0-9A-Za-z.\-]/, '_') || 'unknown_title'
+    video_name= page.title.gsub(/[^0-9A-Za-z.\-]/, '_') || 'unknown_title'
     conference = page.uri.path.split('/')[2] || 'unknown'
 
     page.links.each do |l|
       url = l.uri.to_s
       next unless url =~ /(avi|mp4|ogg|mp3|mov)$/
 
-      save_file url, "#{conference}/#{video_name}#{File.extname(url)}"
+      save_file url, "#{conference}/#{video_name}#{File.extname(url)}", page.title
     end
   end
 end
