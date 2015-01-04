5448f394b65dd7ba274d7d7ce045f3256f1816c2
Sergio Rubio <rubiojr@frameos.org>
2015-01-04 01:14:31 +0100

    firewall, config.txt, more packages

--- rpi/setup-rpi.sh
+++ rpi/setup-rpi.sh
@@ -10,7 +10,8 @@ export DEBIAN_FRONTEND=noninteractive
 
 apt-get install -y openssh-server vim tinc \
                    openvpn ntp unattended-upgrades \
-                   ntpdate wicd-curses curl avahi-daemon
+                   ntpdate wicd-curses curl avahi-daemon \
+                   ufw tcpdump nmap stress
 
 apt-get remove -y --purge libgtk*
 apt-get autoremove -y --purge
@@ -35,9 +36,29 @@ service rsyslog restart
 service ntp start
 service ssh restart
 
+ufw allow 22/tcp
+ufw enable
+
 cat > /etc/apt/apt.conf.d/20auto-upgrades <<EOF
 APT::Periodic::Update-Package-Lists "1";
 APT::Periodic::Unattended-Upgrade "1";
+EOF
+
+cat > /boot/config.txt <<EOF
+#uncomment to overclock the arm. 700 MHz is the default.
+arm_freq=800
+# NOOBS Auto-generated Settings:
+hdmi_force_hotplug=1
+config_hdmi_boost=4
+overscan_left=24
+overscan_right=24
+overscan_top=16
+overscan_bottom=16
+disable_overscan=0
+core_freq=250
+sdram_freq=450
+over_voltage=2
+gpu_mem=16
 EOF
 
 service rsyslog stop
@@ -51,3 +72,4 @@ echo "* Change the pi user password"
 echo "* Fix the hostname"
 echo "* Tinc setup"
 echo "* Wifi config?"
+echo "* Reboot"
