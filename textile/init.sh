e71394b9e103e3d634b082dd6dcc9b94bcafc072
Sergio Rubio <rubiojr@frameos.org>
2020-01-11 22:10:12 +0100

    Add textile scripts

---
+++ textile/init.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+set -x
+
+apt update
+apt install --yes vim build-essential curl wget openssh-client procps sudo
+adduser --disabled-password rubiojr --gecos foo || true
+mkdir -p /home/rubiojr/tmp; cd /home/rubiojr/tmp
+tar -xzvf ../go-textile_v0.7.7_linux-amd64.tar.gz
+./install
+rm -rf /home/rubiojr/tmp/*
+
+su -l -c "textile daemon" rubiojr
+#tail -f /dev/null
