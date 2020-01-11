7ae2b2e842dc642946d76bcb7ea36d9de90a839d
Sergio Rubio <rubiojr@frameos.org>
2020-01-11 23:21:27 +0100

    Install lsof and less

--- textile/init.sh
+++ textile/init.sh
@@ -3,7 +3,7 @@ set -e
 set -x
 
 apt update
-apt install --yes vim build-essential curl wget openssh-client procps sudo
+apt install --yes vim build-essential curl wget openssh-client procps sudo lsof less
 adduser --disabled-password rubiojr --gecos foo || true
 mkdir -p /home/rubiojr/tmp; cd /home/rubiojr/tmp
 tar -xzvf ../go-textile_v0.7.7_linux-amd64.tar.gz
